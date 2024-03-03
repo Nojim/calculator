aeeb5c9880dc6da88b51c1831eb4ea24f954d377
Nojim <oscar8nole@gmail.com>
2024-03-03 17:25:41 +0100

    completed project

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let firstOperand = "";
 let secondOperand = "";
-let currentOperation = "";
+let currentOperation = null;
 let shouldResetScreen = false;
 
 const numberButtons = document.querySelectorAll("[data-number]");
 const operatorButtons = document.querySelectorAll("[data-operator]");
-const equalsButton = document.getElementById("equalisBtn");
+const equalsButton = document.getElementById("equalsBtn");
 const clearButton = document.getElementById("clearBtn");
 const deleteButton = document.getElementById("deleteBtn");
 const pointButton = document.getElementById("pointBtn");
 const lastOperationScreen = document.getElementById("lastOperationScreen");
-const currentOperationScreen = document.getElementById("lastOperationScreen");
+const currentOperationScreen = document.getElementById(
+  "currentOperationScreen"
+);
 
 window.addEventListener("keydown", handleKeyboardInput);
 equalsButton.addEventListener("click", evaluate);
@@ -70,7 +72,7 @@ function setOperation(operator) {
 function evaluate() {
   if (currentOperation === null || shouldResetScreen) return;
   if (currentOperation === "÷" && currentOperationScreen.textContent === "0") {
-    alert("You can´t divide by 0!");
+    alert("You can't divide by 0!");
     return;
   }
   secondOperand = currentOperationScreen.textContent;
@@ -85,4 +87,53 @@ function roundResult(number) {
   return Math.round(number * 1000) / 1000;
 }
 
+function handleKeyboardInput(e) {
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+  if (e.key === ".") appendPoint();
+  if (e.key === "=" || e.key === "Enter") evaluate();
+  if (e.key === "Backspace") deleteNumber();
+  if (e.key === "Escape") clear();
+  if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+    setOperation(convertOperator(e.key));
+}
 
+function convertOperator(keyboardOperator) {
+  if (keyboardOperator === "/") return "÷";
+  if (keyboardOperator === "*") return "×";
+  if (keyboardOperator === "-") return "−";
+  if (keyboardOperator === "+") return "+";
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "−":
+      return substract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
+}
